9cecc50ed818b23aa553d2b5a34413027a26156d
EAVI <CaptainMagnar@gmail.com>
2019-01-26 21:06:59 -0800

    Fixed the clicking effects on music loops.

--- com.bat
+++ com.bat
@@ -11,14 +11,16 @@ echo Compiling Release Build..
 call devenv Ninja.sln /build Release
 echo.
 echo Copying Files
-xcopy "Ninja/SFX" "Debug/SFX" /y /E
-xcopy "Ninja/GFX" "Debug/GFX" /y /E
-xcopy "Ninja/data" "Debug/data" /y /E
-xcopy "Ninja/Screenshots" "Debug/Screenshots" /y /E
-xcopy "Ninja/SFX" "Release/SFX" /y /E
-xcopy "Ninja/GFX" "Release/GFX" /y /E
-xcopy "Ninja/data" "Release/data" /y /E
-xcopy "Ninja/Screenshots" "Release/Screenshots" /y /E
+xcopy "Ninja/SFX" "Debug/SFX" /y /i /E
+xcopy "Ninja/GFX" "Debug/GFX" /y /i /E
+xcopy "Ninja/data" "Debug/data" /y /i /E
+xcopy "Ninja/Screenshots" "Debug/Screenshots" /y /i /E
+xcopy "Ninja/gamecontrollerdb.txt" "Debug/gamecontrollerdb.txt" /y /E
+xcopy "Ninja/SFX" "Release/SFX" /y /i /E
+xcopy "Ninja/GFX" "Release/GFX" /y /i /E
+xcopy "Ninja/data" "Release/data" /y /i /E
+xcopy "Ninja/Screenshots" "Release/Screenshots" /y /i /E
+xcopy "Ninja/gamecontrollerdb.txt" "Release/gamecontrollerdb.txt" /y /E
 echo.
 echo Mission Complete
 ::pause so you can see the compilation errors if there are any
